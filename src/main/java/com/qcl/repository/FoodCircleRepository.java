814c35a3cb9a88f99b7c3bd1ab1ccdebfb7fa7b4
kbwoptimus <244780463@qq.com>
2020-05-19 11:42:34 +0800

    1.0

---
+++ src/main/java/com/qcl/repository/FoodCircleRepository.java
@@ -0,0 +1,12 @@
+package com.qcl.repository;
+
+import com.qcl.bean.FoodCircle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FoodCircleRepository extends JpaRepository<FoodCircle,Integer> {
+    List<FoodCircle> findAllByOpenid(String openid);
+//    List<FoodCircle> findByImageUrl(String image_url);
+}
